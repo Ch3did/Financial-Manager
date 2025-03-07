2bae0bd508b03896e88fb3e00437f022879400c8
Ch3did <kphch3did@gmail.com>
2025-03-07 15:01:13 -0300

    fix: update exceptions

--- src/helpers/exception.py
+++ src/helpers/exception.py
@@ -1,23 +1,11 @@
-class ATMException(Exception):
-    def __init__(
-        self,
-        id=None,
-        message=None,
-        can_retry=True,
-        retries=0,
-        details=None,
-        description=None,
-        expected=None,
-    ):
+class BasicException(Exception):
+    def __init__(self, message):
         self.message = message
-        self.can_retry = can_retry
-        if retries >= 1:
-            self.can_retry = False
-        if id:
-            self.id = id
-        if details:
-            self.details = details
-        if description:
-            self.description = description
-        if expected:
-            self.expected = expected
+class FinancialExeption(BasicException):
+    ...
+    
+class ExtractionException(BasicException):
+    ...
+    
+class DatabaseException(BasicException):
+    ...
