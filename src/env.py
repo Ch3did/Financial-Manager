d7d5313e7eeabc6f93f9f5aecf0068ac49a1409b
Ch3did <kphch3did@gmail.com>
2025-03-07 12:45:14 -0300

    fix; removed unused variables

--- src/env.py
+++ src/env.py
@@ -11,14 +11,8 @@ DEBUG = os.environ.get("DEBUG")
 # Nubank Data
 FOLDER_PATH = os.environ.get("FOLDER_PATH")
 
-USER_NAME = ""
 # DB Conection
 DB_NAME = os.environ.get("DB_NAME")
-# DB_USER = os.environ.get("DB_USER")
-# DB_PASSWORD = os.environ.get("DB_PASSWORD")
-# DB_HOST = os.environ.get("DB_HOST")
-# DB_PORT = os.environ.get("DB_PORT")
-
 DATABASE_URL = f"sqlite:///{DB_NAME}.db"
 
 
