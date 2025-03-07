5cc7330e8f38f783c38c75cf901248c1bff63b59
Ch3did <kphch3did@gmail.com>
2025-03-07 13:24:47 -0300

    wip: added methods to database

--- src/controller/database_controller.py
+++ src/controller/database_controller.py
@@ -1,9 +1,7 @@
-import arrow
 from loguru import logger
 from sqlalchemy import and_
 
 from src.helpers.connection import Connection
-from src.helpers.exception import ATMException
 from src.models.category import Category
 from src.models.register import OFXRegister
 from src.models.transaction import Transaction
@@ -13,51 +11,67 @@ class Database:
     def __init__(self):
         self.conn = Connection()
 
-    def _add_debit(self, transaction):
+    def _add_debit(self, transaction: Transaction) -> None:
         with self.conn.make_session()() as session:
-            if not self.transaction_exists(transaction):
+            if not self._transaction_exists(transaction):
                 session.add(transaction)
-                logger.info(f"Inserido no OFXHandlero: {transaction['title']}")
+                logger.debug(f"Inserida nova Transação: {transaction.description}")
                 session.commit()
 
-    def _transaction_exists(self, incomming_transaction):
+    def _add_register(self, register: OFXRegister):
+        with self.conn.make_session()() as session:
+            session.add(register)
+            logger.debug("Inserido novo Registro de Importação")
+            session.commit()
+
+    def _add_category(self, category: Category):
+        with self.conn.make_session()() as session:
+            if not self._category_exists(category):
+                session.add(category)
+                session.commit()
+
+    def _category_exists(self, incomming_category: Category) -> bool:
+        with self.conn.make_session()() as session:
+            exists = (
+                session.query(Category)
+                .filter((Category.name == incomming_category))
+                .first()
+            )
+        return bool(exists)
+
+    def _transaction_exists(self, incomming_transaction: Transaction) -> bool:
         with self.conn.make_session()() as session:
             exists = (
                 session.query(Transaction)
                 .filter(
                     and_(
-                        Transaction.checknum == incomming_transaction.checknum,
-                        Transaction.detail == incomming_transaction.detail,
+                        Transaction.description == incomming_transaction.description,
                         Transaction.date == incomming_transaction.date,
-                        Transaction.establishment
-                        == incomming_transaction.establishment,
-                        Transaction.typename == incomming_transaction.typename,
-                        Transaction.amount == incomming_transaction.amount,
-                        Transaction.is_negative == incomming_transaction.is_negative,
+                        Transaction.value == incomming_transaction.value,
+                        Transaction.transaction_type
+                        == incomming_transaction.transaction_type,
+                        Transaction.transaction_id
+                        == incomming_transaction.transaction_id,
+                        Transaction.organization == incomming_transaction.organization,
+                        Transaction.org_id == incomming_transaction.org_id,
+                        Transaction.account == incomming_transaction.account,
                     )
                 )
                 .first()
             )
         return bool(exists)
 
-    def _add_register(self, register: OFXRegister):
-        with self.conn.make_session()() as session:
-            session.add(register)
-            logger.info(f"Inserido no OFXHandlero: {register['title']}")
-            session.commit()
-
-    def _get_transactions(self, limit: int = 10):
+    def _get_transactions_with_limit(self, limit: int = 10):
         with self.conn.make_session()() as session:
             return (
-                session.query(Transaction, Category)
-                .join(Category, Transaction.category == Category.id)
-                .order_by(Transaction.date)
-                .limit(limit)
-                .all()
+                session.query(Transaction).order_by(Transaction.date).limit(limit).all()
             )
 
     def _get_transactions_without_category(self):
         with self.conn.make_session()() as session:
             return (
-                session.query(Transaction).filter(Transaction.category_id == None).all()
+                session.query(Transaction).filter(Transaction.category_id is None).all()
             )
+
+    def _get_category_list(self):
+        return self.conn.query(Category).all()
