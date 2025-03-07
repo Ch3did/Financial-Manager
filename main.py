bc842c4727a1c10b9686718cf4bb0d30ec142820
Ch3did <kphch3did@gmail.com>
2025-03-07 13:23:38 -0300

    fix: solve top argument

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import arrow
 import click
 
 from src.views.category_view import CategoryView
@@ -15,23 +14,18 @@ def config(): ...
 @config.command("home", help="Print the home screen")
 @click.option("-t", help="Specifies a type of search from Categories", default=1)
 def home(t):
-    ConfigView.make_homescreen(t)
+    ConfigView().make_homescreen(t)
 
 
 @config.command("migrate", help="Run migrations")
 def run_migrations():
-    ConfigView.run_migrate()
+    ConfigView().run_migrate()
 
 
 # Transactions
 #     ___     ___     ___     ___     ___     ___     ___     ___     ___
 @config.command("top", help="Return the first top transactions")
-@click.option(
-    "--results",
-    "-r",
-    help="Number os results for the output",
-    default="10",
-)
+@click.argument("results", default="10", type=click.INT, required=False)
 def top_transactions(results):
     TransactionsView().return_top_transactions(results)
 
@@ -48,12 +42,12 @@ def update_transactions(path):
 #     ___     ___     ___     ___     ___     ___     ___     ___     ___
 @config.command(help="Get categories list")
 def get_category_info():
-    CategoryView.get_categories()
+    CategoryView().get_categories()
 
 
 @config.command("create", help="Register a new category")
 def create_category():
-    CategoryView.create_category()
+    CategoryView().new_category()
 
 
 if __name__ == "__main__":
