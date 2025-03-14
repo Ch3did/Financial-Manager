19dc2bc3fe0c7e9eedee4add3f94e999d4b56fae
Ch3did <kphch3did@gmail.com>
2025-03-14 11:25:44 -0300

    fix: solve install

--- install.sh
+++ install.sh
@@ -6,27 +6,24 @@ sudo apt update
 # Define constants
 APP_NAME="fmanager"
 INSTALL_DIR="/opt/$APP_NAME"
-VENV_DIR="$INSTALL_DIR/venv"
-ENTRY_SCRIPT="$INSTALL_DIR/run.sh"
-TEMP="/"var/tmp""
 PROFILE_FILE="$HOME/.zshrc"
 
 # Clone your library into /opt
 echo "Setting up the application in $INSTALL_DIR..."
 sudo mkdir -p "$INSTALL_DIR"
-sudo cp -r ./* "$INSTALL_DIR"
+sudo echo -r ./* "$INSTALL_DIR/enviroment.conf >> .env"
+sudo cp "$INSTALL_DIR"
 sudo chown -R $USER:$USER "$INSTALL_DIR"
 
 # Create a virtual environment install dependencies
 echo "Creating a virtual environment..."
-python3 -m venv "$VENV_DIR"
+python3 -m venv "$INSTALL_DIR/venv"
 echo "Installing python dependencies on venv..."
-source "$VENV_DIR/bin/activate"
+source "$INSTALL_DIR/venv/bin/activate"
 pip3 install -r "$INSTALL_DIR/requirements.txt"
-deactivate
 
 echo "Creating an alias to run the application..."
-ALIAS_COMMAND="alias $APP_NAME='$ENTRY_SCRIPT'"
+ALIAS_COMMAND="alias $APP_NAME='$INSTALL_DIR/run.sh'"
 if ! grep -q "$ALIAS_COMMAND" "$PROFILE_FILE"; then
     echo "$ALIAS_COMMAND" >> "$PROFILE_FILE"
     echo "Alias added to $PROFILE_FILE. Use '$APP_NAME' to run the application."
@@ -37,3 +34,9 @@ fi
 # Reload the shell profile
 echo "Reloading shell profile..."
 source "$PROFILE_FILE"
+
+# Create Database
+fmanager migrate
+
+# Deactivate venv
+deactivate
