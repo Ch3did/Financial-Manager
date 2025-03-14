940842c560a9628e01326a13cc6e9ce756c062b9
Ch3did <kphch3did@gmail.com>
2025-03-14 14:27:13 -0300

    fix: rollback clear

--- src/helpers/clear.py
+++ src/helpers/clear.py
@@ -2,13 +2,13 @@ import os
 
 
 def clean_output(func, bypass_hit_enter: bool = False):
-    # os.system("clear")
+    os.system("clear")
 
     def decorator(*args):
         func(*args)
         if bypass_hit_enter:
             input("Press Enter to continue...")
-            # os.system("clear")
+            os.system("clear")
         return
 
     return decorator
