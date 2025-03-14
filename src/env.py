8ebbdac6498be3741a9c715862fe2e3f6add6469
Ch3did <kphch3did@gmail.com>
2025-03-14 14:24:55 -0300

    fix; update database path

--- src/env.py
+++ src/env.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 from sqlmodel import create_engine
 
-load_dotenv("monetary_maid.config")
+load_dotenv(".env")
 
 # Basics
 DEBUG = os.environ.get("DEBUG")
@@ -14,7 +15,7 @@ FOLDER_PATH = os.environ.get("FOLDER_PATH")
 # DB Conection
 DB_NAME = os.environ.get("DB_NAME")
 
-DATABASE_URL = f"sqlite:///{Path.home()}/.local/temp/{DB_NAME}.db"
+DATABASE_URL = f"sqlite:///{Path.home()}/.local/{DB_NAME}.db"
 
 
 engine = create_engine(DATABASE_URL, echo=bool(DEBUG))
